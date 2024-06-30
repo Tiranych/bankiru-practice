b66473619a2187c5578114f5af9396ed56b31998
Tiranych <krimyan2015@yandex.ru>
2024-06-30 13:56:21 +0300

    final commit

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("Мой первый проект на Node JS");
+async function addToArr(array, urls, maxRequests) {
+    for (let curRequests = 0; curRequests < maxRequests; curRequests++) {
+        let fetchRes = fetch(urls[curRequests]);
+        array.push(fetchRes);
+    }
+}
+
+async function makeRequests(urls, maxRequests) {
+    let promisesArr = [];
+    addToArr(promisesArr, urls, maxRequests);
+    while (promisesArr.length != urls.length) {
+        if(Promise.race(promisesArr)) {   
+            addToArr(promisesArr, urls, maxRequests);
+        }
+    }
+    return promisesArr;
+}
+
+let urls = [
+    'https://github.com/Tiranych/Finsweet',
+    'https://github.com/Tiranych/SMT',
+    'https://github.com/Tiranych/Lease-cars',
+    'https://github.com/Tiranych/bankiru-practice',
+    'https://github.com/Tiranych/Parallax-effect',
+    'https://github.com/Tiranych/Dior'
+];
+
+let maxRequests = 2;
+
+let result = makeRequests(urls, maxRequests)
+.then(response => response.forEach(element => {
+    element.then(response => console.log(response));
+}));
